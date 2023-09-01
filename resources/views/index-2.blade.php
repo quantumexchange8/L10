0751ea77c195a2b2f64de5a05528e6d0ad41bbe0
haikal <rajahaikal7@gmail.com>
2023-09-01 15:07:10 +0800

    favicon update

--- resources/views/index-2.blade.php
+++ resources/views/index-2.blade.php
@@ -7,7 +7,7 @@
             Vanward International
         </title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.png') }}">
+        <link rel="icon" href="{{ asset('assets/img/fav.png') }}">
         <style>
             #loader {
                 width: 70px;
