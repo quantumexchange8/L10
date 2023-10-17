35b05664f5f36e6256e0f3406aeaa2167b5ab441
tonglim <tonglim88@gmail.com>
2023-10-17 18:55:57 +0800

    Update _head.blade.php

--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -1,7 +1,9 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="">
+<meta name="description" content="We take a unique approach to investing, providing customised strategies based on the goals of each client and our extensive knowledge of global markets and economies. Through our expert interpretation of multiple data sources, we identify suitable solutions for you, solutions that will strengthen your position. ">
+<meta name="author">
+<meta name="keywords" content="Vanward International, Vanward, Private Investment, Private Equity, Stock Market, Retire Plan, Wealth Planning, Fixed Income">
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap-grid.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/glide.css') }}">
